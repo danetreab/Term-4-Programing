841daa7ef30af95538b6e08e41ee74ff7b631f2c
Hello <deftqml@gmail.com>
2022-03-17 00:10:25 +0700

    K

--- Java-fx/Login-Page/src/application/categoryController.java
+++ Java-fx/Login-Page/src/application/categoryController.java
@@ -1,5 +1,27 @@
 package application;
 
-public class categoryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class categoryController implements Initializable {
     
+    @FXML
+    private ComboBox<String> comb;
+
+    @FXML
+    void comboBoxupdate(ActionEvent event) {
+        String s = comb.getSelectionModel().getSelectedItem().toString();
+    }
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        ObservableList<String> list = FXCollections.observableArrayList("Active","Deactive");
+        comb.setItems(list);
+    }
 }
